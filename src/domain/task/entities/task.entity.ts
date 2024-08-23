a65a7bd43ac69d3991603d2f6d8c5d0fbb02cf0e
Martin Elias <martin.elias@swan.io>
2024-08-23 09:12:54 +0200

    feat/add-gateways

--- src/domain/task/entities/task.entity.ts
+++ src/domain/task/entities/task.entity.ts
@@ -3,13 +3,15 @@ import { randomUUID } from "node:crypto";
 export type Task = {
   id: string;
   status: Status;
+  title: string;
 };
 
-type Status = "Failed" | "Pending" | "Accepted";
+export type Status = "Failed" | "Pending" | "Accepted";
 
 export function createTask(input: Omit<Task, "id">): Task {
   return {
     id: randomUUID(),
     status: input.status,
+    title: input.title,
   };
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,1 @@
+export const TASK_CLIENT = "task-client";
