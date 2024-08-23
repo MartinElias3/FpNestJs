fd2167ee7586dd160de4f3d53807408ce87306e1
Martin Elias <martin.elias@swan.io>
2024-08-23 11:29:06 +0200

    feat/promise

--- src/infrastructure/task/task-client.ts
+++ src/infrastructure/task/task-client.ts
@@ -5,11 +5,20 @@ import {
   FailedTaskDeliveryError,
   TaskProviderGateway,
 } from "src/domain/task/gateways/task-provider.gateway.";
+import { match } from "ts-pattern";
 
 @Injectable()
 export class TaskClient implements TaskProviderGateway {
+  readonly titleThatShouldReject = "ImABadTitle";
   send(task: Task): Future<Result<{ status: Status }, FailedTaskDeliveryError>> {
-    //TODO: need to implement with promise
-    throw new Error("Method not implemented.");
+    return Future.fromPromise(this.simulateAsyncFlow(task.title))
+      .mapError(() => new FailedTaskDeliveryError("Failed to communicate with task client"))
+      .mapOk((response) => response);
+  }
+
+  private simulateAsyncFlow(title: Task["title"]): Promise<{ status: Status }> {
+    return match(title)
+      .with(this.titleThatShouldReject, () => Promise.reject("Bad title !!!"))
+      .otherwise(() => Promise.resolve({ status: "Accepted" as Status }));
   }
 }
